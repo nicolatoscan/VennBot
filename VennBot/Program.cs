7fd91f4ef8f86cc452443cdd2911954491a738b1
Nicola Toscan <nicolatoscan99@outlook.it>
2019-03-06 20:59:59 +0100

    Interface

--- VennBot/Program.cs
+++ VennBot/Program.cs
@@ -3,18 +3,41 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Telegram.Bot;
 
 namespace VennBot
 {
     class Program
     {
-        static void Main(string[] args) {
-            // The code provided will print ‘Hello World’ to the console.
-            // Press Ctrl+F5 (or go to Debug > Start Without Debugging) to run your app.
-            Console.WriteLine("Hello World!");
-            Console.ReadKey();
+        private static readonly TelegramBotClient Bot = new TelegramBotClient("760942295:AAG3bKAaq45TI34KfSiSxgHozwmhsITJsBU");
 
-            // Go to http://aka.ms/dotnet-get-started-console to continue learning how to build a console app! 
+        static void Main(string[] args) {
+            Console.WriteLine("Started");
+            Bot.OnMessage += Bot_OnMessage;
+
+            Bot.StartReceiving();
+            Console.ReadLine();
+            Bot.StopReceiving();
+        }
+
+        private static void Bot_OnMessage(object sender, Telegram.Bot.Args.MessageEventArgs e) {
+
+            List<string> lines = e.Message.Text.Split('\n').ToList();
+
+            List<int> omega = lines[0].Split(' ').Select(Int32.Parse).ToList();
+            lines.RemoveAt(0);
+
+            string formula = lines[lines.Count - 1].Replace(" ", "");
+            lines.RemoveAt(lines.Count - 1);
+
+            Dictionary<char, List<int>> sets = new Dictionary<char, List<int>>();
+            foreach (var l in lines) {
+                var el = l.Split(' ');
+                sets.Add(el[0][0], el.Skip(1).Select(Int32.Parse).ToList());
+            }
+
+
+
         }
     }
 }
